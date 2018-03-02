b3704c39b520b809c84cf177da355df080bbe527
Mafinar Khan <mafinar@gmail.com>
2018-03-01 22:45:24 -0500

    First Blood

---
+++ lib/ex_gps.ex
@@ -0,0 +1,18 @@
+defmodule ExGps do
+  @moduledoc """
+  Documentation for ExGps.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExGps.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_gps_test.exs
@@ -0,0 +1,8 @@
+defmodule ExGpsTest do
+  use ExUnit.Case
+  doctest ExGps
+
+  test "greets the world" do
+    assert ExGps.hello() == :world
+  end
+end
